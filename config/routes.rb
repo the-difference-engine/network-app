7c70590c3e9e92aff7968322f097f3be3120e700
dianacamacho <dcam1901@yahoo.com>
2016-06-12 01:25:04 -0500

    students registrations edit view changes for extra fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'students/index'
-
-  get 'students/show'
 
   devise_for :students
   root "students#index"
+
+  resources :students
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
