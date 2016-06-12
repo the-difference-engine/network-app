35a56ab3027d1bcf33256e9fbf07b6c3a8cdb267
dianacamacho <dcam1901@yahoo.com>
2016-06-12 04:59:24 -0500

    student and project associations

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :follow_up_list
+  has_many :projects
+
 end
