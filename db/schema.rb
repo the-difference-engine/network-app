ff6d317f547b9bca2adddbd21f5246cb10cec6a5
dianacamacho <dcam1901@yahoo.com>
2016-06-12 04:56:03 -0500

    created project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612053738) do
+ActiveRecord::Schema.define(version: 20160612095412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.integer "student_id"
+    t.string  "name"
+    t.text    "description"
+    t.boolean "capstone"
+    t.boolean "client_work"
+    t.string  "github"
+    t.string  "website"
+    t.string  "screencast"
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
