de4cb8df71fa6f3f005dd422d51629e2bf8abdff
Chris Connell <chris.g.connell@gmail.com>
2016-06-12 21:38:00 -0500

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+15.times do 
+  Student.create!(
+    email: Faker::Internet.email,
+    password: "password",
+    avatar: Faker::Company.logo,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    city: Faker::Address.city,
+    industry: Faker::Company.profession,
+    grad_date: "2016-03-25",
+    skill_1: Faker::Hacker.ingverb,
+    skill_2: Faker::Hacker.ingverb,
+    skill_3: Faker::Hacker.ingverb,
+    interest_1: Faker::Hacker.ingverb,
+    interest_2: Faker::Hacker.ingverb,
+    interest_3: Faker::Hacker.ingverb,
+    interview_1: Faker::Hacker.say_something_smart,
+    interview_2: Faker::Hacker.say_something_smart,
+    interview_3: Faker::Hacker.say_something_smart,
+    github: Faker::Internet.url,
+    blog: Faker::Internet.url,
+    quote: Faker::Company.catch_phrase,
+    seeking_employment: random_boolean = [true, false].sample,
+    resume: Faker::Internet.url,
+    )
+end
+
