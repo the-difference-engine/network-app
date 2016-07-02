35f974b5ed9148ca23c25a5373a979f73dc6b09e
dianacamacho <dcam1901@yahoo.com>
2016-07-02 17:15:20 -0500

    student can upload pdf for resume field, show links to resume preview

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -36,9 +36,9 @@ class ResumeUploader < CarrierWave::Uploader::Base
   #   process :resize_to_fit => [50, 50]
   # end
 
-  version :preview do
-    process resize_to_limit: [300,10000]
-  end
+  # version :preview do
+  #   process resize_to_limit: [300,10000]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
