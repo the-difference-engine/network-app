ff16d4a7e2942bd753017ea9b4c3d0dd753f6377
dianacamacho <dcam1901@yahoo.com>
2016-07-06 01:36:00 -0500

    changed other project links to normalized links with link_to

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,15 @@
 class Project < ActiveRecord::Base
   belongs_to :student
+
+  def normalized_github
+    github && PostRank::URI.normalize(github).to_s
+  end
+
+  def normalized_website
+    website && PostRank::URI.normalize(website).to_s
+  end
+
+  def normalized_screencast
+    screencast && PostRank::URI.normalize(screencast).to_s
+  end
 end
