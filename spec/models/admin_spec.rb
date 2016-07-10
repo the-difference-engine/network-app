da3991b876624a7919763eff9a4b1593e0052ef2
dianacamacho <dcam1901@yahoo.com>
2016-07-10 15:29:34 -0500

    validation rspec test with admin factory

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,13 +2,9 @@ require 'rails_helper'
 
 describe Admin do 
   it "is valid with a first name, last name, email, password" do
-    admin = Admin.new(
-      first_name: "Idina",
-      last_name: "Menzel",
-      email: "idina@gmail.com",
-      password: "defying_gravity")
-    expect(admin).to be_valid
+    expect(FactoryGirl.build(:admin)).to be_valid
   end
+
   it "in invalid without a first name"
   it "in invalid without a last name"
   it "returns an admin's full name as a string"
