6350ea87b48b19fb16041463780a84a792de2197
dianacamacho <dcam1901@yahoo.com>
2016-07-12 19:36:14 -0500

    student model github and blog normalized url method tests

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -5,11 +5,25 @@ RSpec.describe Student, :type => :model do
     expect(build(:student)).to be_valid
   end
 
-  it "returns an student's full name as a string" do
-    student = build(:student, 
-      first_name: "Kerry", 
-      last_name: "Wood"
-    )
-    expect(student.full_name).to eq("Kerry Wood")
+  context "with model methods" do
+    it "returns an student's full name as a string" do
+      student = build(:student, 
+        first_name: "Jake", 
+        last_name: "Arrieta"
+      )
+      expect(student.full_name).to eq("Jake Arrieta")
+    end
+
+    it 'returns normalized student github link as a string' do
+      student = build(:student, 
+        github: "m.cubs.mlb.com/player/453562/jake-arrieta")
+      expect(student.normalized_github).to eq("http://m.cubs.mlb.com/player/453562/jake-arrieta")
+    end
+
+    it 'returns normalized student blog link as a string' do
+      student = build(:student, 
+        blog: "m.cubs.mlb.com/player/453562/jake-arrieta")
+      expect(student.normalized_blog).to eq("http://m.cubs.mlb.com/player/453562/jake-arrieta")
+    end
   end
 end
