1901fa6452c28f006afc71c2aeb2078997e0e066
dianacamacho <dcam1901@yahoo.com>
2016-07-12 19:15:51 -0500

    student factory

---
+++ spec/factories/student.rb
@@ -0,0 +1,33 @@
+FactoryGirl.define do 
+  factory :student do 
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    city Faker::Address.city
+    industry Faker::Internet.domain_word
+    grad_date "2015-03-25"
+    skill_1 Faker::Hacker.abbreviation
+    skill_2 Faker::Hacker.abbreviation
+    skill_3 Faker::Hacker.abbreviation
+    interest_1 Faker::Hacker.ingverb
+    interest_2 Faker::Hacker.ingverb
+    interest_3 Faker::Hacker.ingverb
+    interview_1 Faker::Hacker.say_something_smart
+    interview_2 Faker::Hacker.say_something_smart
+    interview_3 Faker::Hacker.say_something_smart
+    github Faker::Internet.url
+    blog Faker::Internet.url
+    quote Faker::Hacker.say_something_smart
+    seeking_employment true
+    resume Faker::Internet.url
+    email Faker::Internet.email
+    password "password"
+
+    factory :invalid_student do 
+      name nil
+    end
+
+    factory :login_student do 
+      email "student_login@gmail.com"
+    end
+  end
+end
