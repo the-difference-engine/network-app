16765da6cacfefc896c2896ba719769f2d4aad7c
dianacamacho <dcam1901@yahoo.com>
2016-07-12 19:43:51 -0500

    student model job_response model method test

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -25,5 +25,17 @@ RSpec.describe Student, :type => :model do
         blog: "m.cubs.mlb.com/player/453562/jake-arrieta")
       expect(student.normalized_blog).to eq("http://m.cubs.mlb.com/player/453562/jake-arrieta")
     end
+
+    it 'returns "Yes" if seeking_employment attribute = true' do
+      student = build(:student, 
+        seeking_employment: true)
+      expect(student.job_response).to eq("Yes")
+    end
+
+    it 'returns "No" if seeking_employment attribute = false' do
+      student = build(:student, 
+        seeking_employment: false)
+      expect(student.job_response).to eq("No")
+    end
   end
 end
