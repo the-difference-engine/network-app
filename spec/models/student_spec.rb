f6d40b6dfd3eaa68c5c7a64bb1bccc98da251204
dianacamacho <dcam1901@yahoo.com>
2016-07-16 06:00:16 -0500

    test for friendly_grad_date student model method

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -113,5 +113,11 @@ RSpec.describe Student, :type => :model do
         interview_3: "")
       expect(student.interview_answers?).to eq(false)
     end
+
+    it 'returns "abbreviated month year" format for student friendly_grad_date method' do
+      student = create(:student,
+        grad_date: "2016-03-25")
+      expect(student.friendly_grad_date).to eq("Mar 2016")
+    end
   end
 end
