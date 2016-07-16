ac793685410e9c2dff987d8f29742a0f25e88a06
dianacamacho <dcam1901@yahoo.com>
2016-07-16 04:39:42 -0500

    student model method to check for skills?

--- app/models/student.rb
+++ app/models/student.rb
@@ -44,32 +44,60 @@ class Student < ActiveRecord::Base
     end
   end
 
+  def skills?
+    skills_answered = false
+
+    if skill_1 == nil || skill_1 == ""
+      skill_1 = false
+    else
+      skill_1 = true
+    end
+
+    if skill_2 == nil || skill_2 == ""
+      skill_2 = false
+     else
+      skill_2 = true
+    end
+
+    if skill_3 == nil || skill_3 == ""
+      skill_3 = false
+    else
+      skill_3 = true
+    end
+
+    if skill_1 || skill_2 || skill_3
+      skills_answered = true
+    end
+
+    return skills_answered
+  end
+
   def interview_answers?
-    @questions_answered = false
+    questions_answered = false
 
     if interview_1 == nil || interview_1 == ""
-      @question_1 = false
+      question_1 = false
     else
-      @question_1 = true
+      question_1 = true
     end
 
     if interview_2 == nil || interview_2 == ""
-      @question_2 = false
+      question_2 = false
      else
-      @question_2 = true
+      question_2 = true
     end
 
     if interview_3 == nil || interview_3 == ""
-      @question_3 = false
+      question_3 = false
     else
-      @question_3 = true
+      question_3 = true
     end
 
-    if @question_1 || @question_2 || @question_3
-      @questions_answered = true
+    if question_1 || question_2 || question_3
+      questions_answered = true
     end
 
-    return @questions_answered
+    return questions_answered
   end
 end
 
