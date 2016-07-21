3278a5c4cc9a00a64b096e67c0b770615e6937ee
dianacamacho <dcam1901@yahoo.com>
2016-07-21 02:03:39 -0500

    added more fields to invites parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   layout 'portfolio'
 
@@ -54,13 +53,56 @@ class ApplicationController < ActionController::Base
 
 protected
 
+  # def configure_permitted_parameters
+  #   # Only add some parameters
+  #   devise_parameter_sanitizer.for(:accept_invitation).concat [:first_name, :last_name, :description, :name, :rep_first_name, :rep_last_name, :rep_phone, :rep_email, :city, :state, :description, :invited_by_id, :invited_by_type]
+  #   # Override accepted parameters
+  #   # devise_parameter_sanitizer.for(:accept_invitation) do |u|
+  #   #   u.permit(:first_name, :last_name, :phone, :password, :password_confirmation,
+  #   #            :invitation_token)
+  # end
+
   def configure_permitted_parameters
-    # Only add some parameters
-    devise_parameter_sanitizer.for(:accept_invitation).concat [:first_name, :last_name, :description, :name, :rep_first_name, :rep_last_name, :rep_phone, :rep_email, :city, :state, :description, :invited_by_id, :invited_by_type]
-    # Override accepted parameters
-    # devise_parameter_sanitizer.for(:accept_invitation) do |u|
-    #   u.permit(:first_name, :last_name, :phone, :password, :password_confirmation,
-    #            :invitation_token)
+    devise_parameter_sanitizer.for(:accept_invitation).concat [
+      :first_name, 
+      :last_name,
+      :avatar,
+      :city,
+      :current_city,
+      :current_state,
+      :industry,
+      :grad_date,
+      :skill_1,
+      :skill_2,
+      :skill_3,
+      :interest_1,
+      :interest_2,
+      :interest_3,
+      :interview_1,
+      :interview_2,
+      :interview_3,
+      :github,
+      :blog,
+      :linked_in,
+      :quote,
+      :seeking_employment,
+      :resume,
+      :follow_up_list_id,
+      :name,
+      :image,
+      :description,
+      :rep_first_name,
+      :rep_last_name,
+      :rep_phone,
+      :rep_email,
+      :website,
+      :state,
+      :email,
+      :password,
+      :password_confirmation,
+      :invited_by_id, 
+      :invited_by_type
+    ]
   end
 
   def authenticate_inviter!
