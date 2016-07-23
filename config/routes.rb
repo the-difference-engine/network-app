3edaee5fa41ed4c0dbd95e02abebbff974ffaf62
dianacamacho <dcam1901@yahoo.com>
2016-07-23 00:17:33 -0500

    resources :positions added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Rails.application.routes.draw do
   
   resources :employers
   resources :projects
+  resources :positions
 
 
   # The priority is based upon order of creation: first created -> highest priority.
