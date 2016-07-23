a92e29f54a818718991ce27c91187bef0a14832e
dianacamacho <dcam1901@yahoo.com>
2016-07-23 02:06:45 -0500

    added currently_hiring boolean, hiring_timeline string, and number_of_positions integer to Employers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723063006) do
+ActiveRecord::Schema.define(version: 20160723065913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,18 +49,18 @@ ActiveRecord::Schema.define(version: 20160723063006) do
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "employers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "name"
     t.string   "image"
     t.text     "description"
@@ -79,6 +79,9 @@ ActiveRecord::Schema.define(version: 20160723063006) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.boolean  "currently_hiring",       default: false
+    t.string   "hiring_timeline"
+    t.integer  "number_of_positions"
   end
 
   add_index "employers", ["email"], name: "index_employers_on_email", unique: true, using: :btree
