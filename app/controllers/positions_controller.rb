c995870b129f427a3fa4718dcbec96991304923c
dianacamacho <dcam1901@yahoo.com>
2016-07-23 00:18:45 -0500

    added admin/employer authentication to positions controller

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,6 +1,6 @@
 class PositionsController < ApplicationController
   before_action :set_position, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_admin_employer!
   # GET /positions
   # GET /positions.json
   def index
