f72bcdc4983f6cbd89a3f54f42840540fe18e4d0
dianacamacho <dcam1901@yahoo.com>
2016-07-24 10:45:32 -0500

    created join table for students and positions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160724151600) do
+ActiveRecord::Schema.define(version: 20160724154432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,11 @@ ActiveRecord::Schema.define(version: 20160724151600) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "positions_students", id: false, force: :cascade do |t|
+    t.integer "position_id", null: false
+    t.integer "student_id",  null: false
   end
 
   create_table "projects", force: :cascade do |t|
