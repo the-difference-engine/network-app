ca52631cb86eac4c905567d47ae51545db301063
dianacamacho <dcam1901@yahoo.com>
2016-07-24 11:52:51 -0500

    renamed student industry attribute to current_industry

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160724154432) do
+ActiveRecord::Schema.define(version: 20160724165056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -187,7 +187,7 @@ ActiveRecord::Schema.define(version: 20160724154432) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "city"
-    t.string   "industry"
+    t.string   "current_industry"
     t.date     "grad_date"
     t.string   "skill_1"
     t.string   "skill_2"
