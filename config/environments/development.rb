4300d00a14b572dc0822f3df210d0cb90cf1422c
dianacamacho <dcam1901@yahoo.com>
2016-07-24 23:52:01 -0500

    changed development.rb default url to localhost:3000

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { host: 'http://network-app-staging.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000'}
   config.action_mailer.delivery_method = :smtp
   
   ActionMailer::Base.smtp_settings = {
