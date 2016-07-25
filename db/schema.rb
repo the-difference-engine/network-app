b0982c93313b39e117504434e1aeb844c6f746a4
dianacamacho <dcam1901@yahoo.com>
2016-07-25 01:21:51 -0500

    added about_me attribute to students

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725050519) do
+ActiveRecord::Schema.define(version: 20160725062103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -222,6 +222,7 @@ ActiveRecord::Schema.define(version: 20160725050519) do
     t.string   "current_city"
     t.string   "current_state"
     t.boolean  "active",                 default: false
+    t.text     "about_me"
   end
 
   add_index "students", ["email"], name: "index_students_on_email", unique: true, using: :btree
