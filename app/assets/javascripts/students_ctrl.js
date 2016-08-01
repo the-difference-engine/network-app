6b919fc9477c17131393824eb791c19bb4c7f896
dianacamacho <dcam1901@yahoo.com>
2016-08-01 03:02:48 -0500

    students angular controller edits

--- app/assets/javascripts/students_ctrl.js
+++ app/assets/javascripts/students_ctrl.js
@@ -17,7 +17,17 @@
           } else  {
             $scope.pendingStudents.push(student);
           }
+
+          // $scope.chunkedStudentData = chunk($scope.activeStudents, 3);
         }
+
+        // function chunk(arr, size) {
+        //     var newArray = [];
+        //     for (var i=0; i<arr.length; i+=size) {
+        //       newArray.push(arr.slice(i, i+size));
+        //     }
+        //     return newArray;
+        //   }
 
         $scope.skillExists = function(student){
           if(student.skill_1 == ""){
@@ -28,19 +38,21 @@
         };
       });
 
-      $http.get("/technologies.json").then(function(response){
-        $scope.technologies = response.data;
-        $scope.results = $scope.technologies;
 
-        $scope.generateList = function(){
-          if($scope.selectedList.length === 0){
-            return $scope.results;
-          } else {
-            $scope.results = $scope.selectedList;
-            return $scope.selectedList;
-          }
-        };
-      }); 
+
+      // $http.get("/technologies.json").then(function(response){
+      //   $scope.technologies = response.data;
+      //   $scope.results = $scope.technologies;
+
+      //   $scope.generateList = function(){
+      //     if($scope.selectedList.length === 0){
+      //       return $scope.results;
+      //     } else {
+      //       $scope.results = $scope.selectedList;
+      //       return $scope.selectedList;
+      //     }
+      //   };
+      // }); 
     };
 
     window.scope = $scope;
