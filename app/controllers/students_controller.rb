22d29f5fcd67efb9ab0d67714437a13370c48a76
Chris Connell <chris.g.connell@gmail.com>
2016-08-02 15:22:49 -0500

    error handling for student emails

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -98,13 +98,32 @@ class StudentsController < ApplicationController
   end
 
   def batch_invite
-    @student_array = Array.new
+    @invite_send_success = Array.new
+    @invite_send_errors = Array.new
+    @invite_send_duplicate = Array.new
     params[:student_emails].split(",").each do |unparsed_email|
       if parsed_email = EmailAddress.new(unparsed_email).parse_email
+        if Student.find_by(email: parsed_email.to_s)
+          @invite_send_duplicate << unparsed_email
+        else
         Student.create(:email => parsed_email, :invited_by_id => current_user.id, :invited_by_type => current_user.class).invite!
-      @student_array << parsed_email
+        @invite_send_success << parsed_email
+        end
+      else
+        @invite_send_errors << unparsed_email
       end
-    flash[:success] = "Invitations sent to #{@student_array.to_sentence}!"
+      
+      if @invite_send_success
+        flash[:success] = "Invitation(s) successfully sent to #{@invite_send_success.to_sentence}"
+      end
+
+      if @invite_send_errors
+        flash[:warning] = "Invitation(s) not sent to #{@invite_send_errors.to_sentence}"
+      end
+
+      if @invite_send_duplicate
+        flash[:warning] = "Invitation(s) not sent to #{@invite_send_duplicate.to_sentence}"
+      end
     end
     redirect_to "/"
   end
