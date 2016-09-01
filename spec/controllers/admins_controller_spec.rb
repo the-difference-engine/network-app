b7871eab7c243cfc9d91336dec2dec8572536b30
dianacamacho <dcam1901@yahoo.com>
2016-09-01 14:57:51 -0500

    admins controller create tests

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -42,4 +42,49 @@ RSpec.describe AdminsController, :type => :controller do
       expect(response).to redirect_to students_path
     end
   end
+
+  describe 'POST #create' do
+    context "when admin is signed in and valid admin attributes" do
+      sign_in_admin
+
+      it "should save the new admin in the database" do   
+        expect {
+          post :create, admin: attributes_for(:admin)
+        }.to change(Admin, :count).by(1)
+      end
+
+      it "should redirect to admins#admin_center" do
+        post :create, admin: attributes_for(:admin)
+        expect(response).to redirect_to admin_center_path
+      end
+    end
+
+    # context "when admin is signed in and invalid student attributes" do 
+    #   sign_in_admin
+
+    #   it "should not save the student in the database" do   
+    #     expect {
+    #       post :create, student: attributes_for(:invalid_student)
+    #     }.not_to change(Student, :count)
+    #   end
+
+    #   it "should re-render student :new template" do 
+    #     post :create, student: attributes_for(:invalid_student)
+    #     expect(response).to render_template :new
+    #   end
+    # end
+
+    # context "when admin is not signed in" do
+    #   it "should not save the new student in the database" do   
+    #     expect {
+    #       post :create, student: attributes_for(:student)
+    #     }.to change(Student, :count).by(0)
+    #   end
+
+    #   it "should redirect to /sign_in" do
+    #     post :create, student: attributes_for(:student)
+    #     expect(response).to redirect_to home_sign_in_path
+    #   end
+    # end
+  end
 end
