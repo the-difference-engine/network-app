67d27d2cff3fcedb0132b2e883ff75605d1e2473
Chris Connell <chris.g.connell@gmail.com>
2016-09-18 21:24:46 -0500

    added flash message after batch submission

--- app/controllers/batch_invitations_controller.rb
+++ app/controllers/batch_invitations_controller.rb
@@ -2,6 +2,9 @@ class BatchInvitationsController < ApplicationController
   
   def create
     @batch = BatchInvitation.create(unparsed_emails: params[:email_addresses], invitee_type: params[:type], invited_by_id: current_user.id, invited_by_type: current_user.class)
+      
+    flash[:info] = "Invitations processing ..."
+
     Resque.enqueue(BackgroundInvitationEmailer, @batch.id)
     
     redirect_to "/"
