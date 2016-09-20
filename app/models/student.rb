ada3a2121dd48b3d5064df6c15ed5fa89e1eff55
dianacamacho <dcam1901@yahoo.com>
2016-09-20 11:43:51 -0500

    validations for top 3 skills

--- app/models/student.rb
+++ app/models/student.rb
@@ -16,7 +16,7 @@ class Student < ActiveRecord::Base
   has_many :employer_email_records, dependent: :destroy
   has_many :student_email_records, dependent: :destroy
 
-  validates :first_name, :last_name, :city, :grad_date, presence: true
+  validates :first_name, :last_name, :city, :grad_date, :skill_1, :skill_2, :skill_3, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
