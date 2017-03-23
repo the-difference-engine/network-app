ae8c7229f2cb9041b84309c783439a8fbb3f9ec0
dianacamacho <dcam1901@yahoo.com>
2017-03-22 19:40:15 -0500

    dependent destroy for follow up list

--- app/models/follow_up_list.rb
+++ app/models/follow_up_list.rb
@@ -1,6 +1,6 @@
 class FollowUpList < ActiveRecord::Base
   belongs_to :employer
-  has_many :follow_up_students
+  has_many :follow_up_students, dependent: :destroy
   has_many :students, through: :follow_up_students
   
   validates :name, presence: true
